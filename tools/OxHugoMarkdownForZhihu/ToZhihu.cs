42439804d90c0ec85cdd87adc1a1654d9a59f1f2
Bourne Young <young40@gmail.com>
2022-06-14 02:16:13 +0800

    update for blog

--- tools/OxHugoMarkdownForZhihu/ToZhihu.cs
+++ tools/OxHugoMarkdownForZhihu/ToZhihu.cs
@@ -4,6 +4,12 @@ public class ToZhihu
 {
     public static void ParseFile(string file)
     {
+        CleanForHugo(file);
+        UpdateForZhihu(file);
+    }
+    
+    private static void CleanForHugo(string file)
+    {
         if (file.EndsWith(".org"))
         {
             return;
@@ -16,34 +22,73 @@ public class ToZhihu
         c = c.Replace("\\\\[", "$$");
         c = c.Replace("\\\\]", "\\\\\n$$");
         c = c.Replace("\\\\\\\\", "\\\\\\");
-        
-        /*
-        var newLines = new List<string>();
-
-        var remove = false;
-        foreach (var line in lines)
-        {
-            if (line.StartsWith("```plantuml"))
-            {
-                remove = true;
-            }
-
-            if (remove && line.StartsWith("```") && !line.Contains("plantuml"))
-            {
-                remove = false;
-                continue;
-            }
-
-            if (remove)
-            {
-                continue;
-            }
-
-            // Console.WriteLine(line);]
-            newLines.Add(line);
-        }
-        */
 
         File.WriteAllText(file, c);
+    }
+
+    private static void UpdateForZhihu(string file)
+    {
+        if (file.EndsWith(".org"))
+        {
+            return;
+        }
+
+        var c = File.ReadAllText(file);
+        
+        c = c.Replace("\\\\\\", "\\\\");
+
+        string[] lines = c.Split("\n");
+
+        var willSkip = false;
+        for (int i = 0; i < lines.Length; i++)
+        {
+            if (lines[i].StartsWith("#") && lines[i].Contains("{#"))
+            {
+                var lA = lines[i].Split("{#");
+                lines[i] = lA[0];
+            }
+
+            if (!lines[i].Trim().StartsWith("$$") && lines[i].Contains("$"))
+            {
+                lines[i] = lines[i].Replace("$", "$$");
+            }
+
+            var link = "https://github.com/young40/young40.github.com/raw/hugo/static";
+
+            link = "http://121.196.158.145";
+
+            if (lines[i].Contains("figure") && lines[i].Contains("/ox-hugo/"))
+            {
+                var lA = lines[i].Split('"');
+                if (lA.Length > 1)
+                {
+                    lA[1] = lA[1].Replace(".svg", ".png");
+                    lines[i] = "<img src=\"" + link + lA[1] + "\" />";
+                }
+            }
+
+            var hasPlus = lines[i].StartsWith("+++");
+            if (hasPlus && !willSkip)
+            {
+                willSkip = true;
+                lines[i] = "";
+                continue;
+            }
+
+            if (willSkip)
+            {
+                lines[i] = "";
+            }
+
+            if (hasPlus && willSkip)
+            {
+                willSkip = false;
+            }
+        }
+        
+
+        file = file.Replace("/post/", "/zhihu/");
+        Console.WriteLine(file);
+        File.WriteAllLines(file, lines);
     }
 }
